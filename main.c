1b8c8797dd2680823ee994c1160efae0c651ce9f
Shaolei Fan <shaolei.fan@nokia.com>
2014-12-12 11:43:12 +0800

    output more readable

--- main.c
+++ main.c
@@ -11,57 +11,53 @@ void usage();
 
 int main(int argc, char **argv)
 {
-  char *filename, *account, *key, *container, *vhd;
-  FILE *fp;
-  int buffer[512/__SIZEOF_INT__];
-  int index = 0;
-  int i, count;
+    char *filename, *account, *key, *container, *vhd;
+    FILE *fp;
+    int buffer[512/__SIZEOF_INT__];
+    int index = 0;
+    int i, count;
 
-  if (argc != 6) {
-    printf("wrong number of arguments\n");
-    usage();
-    return(1);
-  }
-
-  filename = argv[1];
-  account = argv[2];
-  key = argv[3];
-  container = argv[4];
-  vhd = argv[5];
-
-  fp = fopen(filename, "r");  
-  if (fp == NULL) {
-    fprintf(stderr, "Can't open file %s!\n", filename);
-    return(1);
-  }
-  
-  azure_upload_init();
-
-  while (!feof(fp)) {
-    fread(buffer, 512, 1, fp);
-    for (i = 0; i < 512/__SIZEOF_INT__; i++) {
-      if (buffer[i] != 0) {
-	azure_upload((char *)buffer, index * 512, 512, account, key, container, vhd);	
-	count++;
-	printf("Uploaded page %d\n", index);
-        break;
-      }
+    if (argc != 6) {
+        printf("wrong number of arguments\n");
+        usage();
+        return(1);
     }
-    if (index % 10 == 0) {
-      printf("Scaned %d pages\r", index);
-      fflush(stdout);
+
+    filename = argv[1];
+    account = argv[2];
+    key = argv[3];
+    container = argv[4];
+    vhd = argv[5];
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Can't open file %s!\n", filename);
+        return(1);
     }
-    index++;
-  }
 
-  printf("count = %d\n", count);
+    azure_upload_init();
 
-  azure_upload_cleanup();
+    while (!feof(fp)) {
+        fread(buffer, 512, 1, fp);
+        for (i = 0; i < 512/__SIZEOF_INT__; i++) {
+            if (buffer[i] != 0) {
+                azure_upload((char *)buffer, index * 512, 512, account, key, container, vhd);
+                count++;
+                break;
+            }
+        }
+        if (index % 10 == 0) {
+            printf("Scaned %d pages, uploaded %d pages\r", index, count);
+            fflush(stdout);
+        }
+        index++;
+    }
 
-  fclose(fp);
+    azure_upload_cleanup();
+    fclose(fp);
 }
 
 void usage()
 {
-  printf("Usage: vhd_upload <path> <account> <key> <container> <vhd>\n");
+    printf("Usage: vhd_upload <path> <account> <key> <container> <vhd>\n");
 }
